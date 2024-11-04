e03ac5b4c1a0d1d4a60cee578714814314aafc0d
JayZenith <jayzenith248@gmail.com>
2024-11-04 12:40:26 -0800

    tokenized the input string

--- testing/src/tokenization.hpp
+++ testing/src/tokenization.hpp
@@ -4,11 +4,11 @@
 #include <string>
 #include <vector>
 
-enum class Tokens{ leave, lp, intVal, rp };
+enum class Tokens{ leave, lp, intVal, rp, semi };
 
 struct TokensStruct{
     Tokens type;
-    std::optional<Tokens> value {};
+    std::optional<std::string> value {};
 
 };
 
@@ -25,14 +25,62 @@ public:
         std::string buf = "";
         
 
-        while(peak().has_value()){
-            if(isalpha(peak().value())){
-                buf.push_back(peak().value());
-                consume();
-                std::cout << "Here" << std::endl;
-            }
+        if(peak().has_value() && isalpha(peak().value())){ //Make sure first character is correct 
+            buf.push_back(consume()); //PUSH VALUE AND INCREMENT COUNT
+            //consume();
+            while(peak().has_value()){ //for remainder of content  //can take letters and numbers 
+            
+                
+                if(isalpha(peak().value())){
+                    std::cout << peak().value() << std::endl;
+                    buf.push_back(consume()); //PUSH VALUE AND INCREMENT COUNT
+                    if(buf == "leave"){
+                        tokens.push_back({.type=Tokens::leave});
+                        buf.clear();
+                        continue;
+                        //std::cout << "Here" << std::endl;
+
+                    }
+                } //end if 
+                else if(peak().value() == '('){
+                    buf.push_back(consume()); //PUSH VALUE AND INCREMENT COUNT
+                    tokens.push_back({.type=Tokens::lp});
+                    buf.clear();
+                    continue;
+                }
+                else if(isdigit(peak().value())){
+                    std::cout << "made it" << std::endl;
+                    while(isdigit(peak().value())){
+                        buf.push_back(consume()); //PUSH VALUE AND INCREMENT COUNT
+                    }
+                    tokens.push_back({.type=Tokens::intVal, .value=buf});
+                    buf.clear();
+                    continue;
+                }
+                else if(peak().value() == ')'){
+                    buf.push_back(consume()); //PUSH VALUE AND INCREMENT COUNT
+                    tokens.push_back({.type=Tokens::rp});
+                    buf.clear();
+                    continue;
+                }
+                else if(peak().value() == ';'){
+                    buf.push_back(consume()); //PUSH VALUE AND INCREMENT COUNT
+                    tokens.push_back({.type=Tokens::semi});
+                    buf.clear();
+                    continue;
+                }
+
+            } //end while
+            
+                
+        } else {
+            std::cerr << "Not a leave statement" << std::endl;
+            exit(EXIT_FAILURE);
         }
 
+        idx = 0;
+        std::cout << "Here" << std::endl;
+        return tokens;
     }
 
 private:
@@ -46,9 +94,9 @@ private:
         }
     }
 
-    std::optional<char> consume(){
+    char consume(){
         if(idx > contents.length() - 1){  //leave(9); length: 9 but length: 8 with indexing. Thus, if idx > 8 stop.
-            return {};
+            return 0;
         } else {
             return contents[idx++]; //return character and increment index 
         }
